51123a66332664c37c06ac7a8ce0595db14344aa
vagrant <vagrant@precise32.(none)>
2016-01-21 19:36:25 +0000

    fixed bug

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,13 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("Javascript is connected");
 		$("#testjs").text("Please wait...");
-		$(".jumbotron p").addClass("active");
+		//$(".jumbotron p").addClass("active");
 		$(".jumbotron p").toggleClass("active");
-		$("#submitBtn").click(updateProject); 
 	});
-
+    $("#submitBtn").click(updateProject);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
